c7a560ba63aa6f3063180b79911fc0e2b3627736
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-07-20 16:01:03 -0300

    Req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,17 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # https://www.pythontutorial.net/python-oop/python-__eq__/
+    assert Ingredient("ovo").__eq__(Ingredient("ovo"))
+
+    # https://www.pythontutorial.net/python-oop/python-__hash__/
+    assert Ingredient("ovo").__hash__() == hash("ovo")
+
+    assert Ingredient("ovo").name == "ovo"
+
+    # https://www.pythontutorial.net/python-oop/python-__repr__/
+    assert Ingredient("ovo").__repr__() == "Ingredient('ovo')"
+
+    assert Ingredient("ovo").restrictions == {Restriction.ANIMAL_DERIVED}
